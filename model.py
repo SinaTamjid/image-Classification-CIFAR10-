ff861b4887c80db1ad393adb28fe89e30e86738e
SinaTamjid <sinatamjidfts1234@gmail.com>
2025-03-13 20:58:30 +0330

    some changes

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ from keras.api.datasets import cifar10
 from keras.api.utils import to_categorical
 from keras.api.layers import Conv2D,MaxPooling2D,Flatten,Dense,Dropout
 from keras.api.models import Sequential
+from keras._tf_keras.keras.preprocessing.image import ImageDataGenerator
 
 (X_train,y_train),(X_test,y_test)=cifar10.load_data()
 
@@ -11,6 +12,15 @@ X_test=X_test.astype('float')/255.0
 
 y_train=to_categorical(y_train,10)
 y_test=to_categorical(y_test,10)
+
+datagen = ImageDataGenerator(
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip=True
+)
+
+datagen.fit(X_train)
 
 model=Sequential()
                #convolutional layer
@@ -24,12 +34,12 @@ model.add(MaxPooling2D(2,2))
 #connected layer
 model.add(Flatten())
 model.add(Dense(512,activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
 model.add(Dense(10,activation='softmax'))
 
 
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-model.fit(X_train,y_train,epochs=30,verbose=2,batch_size=32,validation_data=(X_test,y_test))
+model.fit(X_train,y_train,epochs=100,verbose=2,batch_size=64,validation_data=(X_test,y_test))
 
 #evaluation
 
